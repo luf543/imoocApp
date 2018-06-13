35b813a1fc70af7064c0be6db0cc53aec5e9e507
lufeng.tuniu <lufeng@tuniu.com>
2018-06-13 18:34:46 +0800

    imooc 7-4

--- app/account/index.js
+++ app/account/index.js
@@ -15,10 +15,10 @@ class Account extends Component {
     return (
       <View style={styles.container}>
         <Text style={[styles.item, styles.item1]}>账户页面1</Text>
-        <View style={[styles.item, styles.item1]}>
+        <View style={[styles.item, styles.item2]}>
           <Text>账户页面2</Text>
         </View>
-        <View style={[styles.item, styles.item1]}>
+        <View style={[styles.item, styles.item3]}>
           <Text>账户页面3</Text>
         </View>
       </View>
@@ -32,9 +32,21 @@ const styles = StyleSheet.create({
     flexDirection: 'row',  //默认column
     flexWrap: 'wrap', //默认nowrap不换行
     justifyContent: 'space-around',  //主轴控制，默认flex-start，两端对齐space-between，两端对齐且两边距离是中间距离一半space-around
-    alignItems: 'center',
+    alignItems: 'center', //stretch拉伸
     backgroundColor: '#ff6600',
-  }
+  },
+  item1: {
+    flex: 1,
+    backgroundColor: '#ccc',
+  },
+  item2: {
+    flex: 1,
+    backgroundColor: '#999',
+  },
+  item3: {
+    flex: 1,
+    backgroundColor: '#666',
+  },
 });
 
 export default Account
