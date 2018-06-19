7df700b32dfdb05237fc390dcdfc393a22f70229
lufeng.tuniu <lufeng@tuniu.com>
2018-06-19 19:18:56 +0800

    imooc 8-3

--- app/account/login.js
+++ app/account/login.js
@@ -11,6 +11,7 @@ import {
   Alert,
 } from 'react-native';
 import Button from 'react-native-button';
+import {CountDownText} from 'react-native-lf-countdown'
 
 
 class Login extends Component {
@@ -19,6 +20,7 @@ class Login extends Component {
     this.state = {
       verifyCode: '',
       phoneNumber: '',
+      countingDone: false,
       codeSent: false,
     }
   }
@@ -26,6 +28,12 @@ class Login extends Component {
   _showVerifyCode(){
     this.setState({
       codeSent: true
+    })
+  }
+
+  _countingDone(){
+    this.setState({
+      countingDone: true
     })
   }
 
@@ -53,8 +61,34 @@ class Login extends Component {
       })
   }
 
+  _submit(){
+    const {phoneNumber, verifyCode} = this.state
+
+    if(!phoneNumber || !verifyCode){
+      return Alert.alert('手机号或验证码不能为空！')
+    }
+
+    const body = {
+      phoneNumber: phoneNumber,
+      verifyCode: verifyCode
+    }
+
+    request.post(config.api.base + config.api.verify, body)
+      .then((data) => {
+        if(data && data.success){
+          console.log('login ok')
+          console.log(data)
+        }else{
+          Alert.alert('获取验证码失败，请检查手机号是否正确')
+        }
+      })
+      .catch((err) => {
+        Alert.alert('获取验证码失败，请检查网络是否良好')
+      })
+  }
+
   render(){
-    const {codeSent} = this.state
+    const {codeSent, countingDone} = this.state
     return (
       <View style={styles.container}>
         <View style={styles.signupBox}>
@@ -76,17 +110,34 @@ class Login extends Component {
             codeSent
             ? <View style={styles.verifyCodeBox}>
                 <TextInput
-                placeholder='输入验证码'
-                autoCapitalize='none'
-                autoCorrect={false}
-                keyboardType='numeric'
-                style={styles.inputField}
-                onChangeText={(text) => {
-                  this.setState({
-                    verifyCode: text
-                  })
-                }}
-              />
+                  placeholder='输入验证码'
+                  autoCapitalize='none'
+                  autoCorrect={false}
+                  keyboardType='numeric'
+                  style={styles.inputVerify}
+                  onChangeText={(text) => {
+                    this.setState({
+                      verifyCode: text
+                    })
+                  }}
+                />
+                {
+                  countingDone
+                  ? <Button
+                    style={styles.countBtn}
+                    onPress={this._sendVerifyCode.bind(this)}>获取验证码</Button>
+                  : <CountDownText
+                      style={styles.countBtn}
+                      countType='seconds' // 计时类型：seconds / date
+                      auto={true} // 自动开始
+                      afterEnd={this._countingDone.bind(this)} // 结束回调
+                      timeLeft={60} // 正向计时 时间起点为0秒
+                      step={-1} // 计时步长，以秒为单位，正数则为正计时，负数为倒计时
+                      startText='获取验证码' // 开始的文本
+                      endText='获取验证码' // 结束的文本
+                      intervalText={(sec) => '剩余秒数:' + sec} // 定时的文本回调
+                    />
+                }
             </View>
             : null
           }
@@ -95,7 +146,7 @@ class Login extends Component {
             codeSent
             ? <Button
               style={styles.btn}
-              onPress={this._submit}>登录</Button>
+              onPress={this._submit.bind(this)}>登录</Button>
             :<Button
               style={styles.btn}
               onPress={this._sendVerifyCode.bind(this)}>获取验证码</Button>
@@ -131,6 +182,36 @@ const styles = StyleSheet.create({
     fontSize: 16,
     backgroundColor: '#fff',
     borderRadius: 4
+  },
+
+  verifyCodeBox: {
+    marginTop: 10,
+    flexDirection: 'row',
+    justifyContent: 'space-between',
+  },
+
+  inputVerify: {
+    flex: 1,
+    height: 40,
+    padding: 5,
+    color: '#666',
+    fontSize: 16,
+    backgroundColor: '#fff',
+    borderRadius: 4
+  },
+
+  countBtn: {
+    width: 110,
+    height: 40,
+    padding: 10,
+    marginLeft: 8,
+    backgroundColor: '#ee735c',
+    borderColor: '#ee735c',
+    color: '#fff',
+    textAlign: 'left',
+    fontWeight: '600',
+    fontSize: 15,
+    borderRadius: 2,
   },
   
   btn: {
