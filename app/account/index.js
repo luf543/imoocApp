2726df9e0923b113c0bea93bc6aef4324e6621e7
lufeng.tuniu <lufeng@tuniu.com>
2018-06-20 15:02:23 +0800

    imooc 9-2

--- app/account/index.js
+++ app/account/index.js
@@ -8,6 +8,8 @@ import {
   AsyncStorage,
   TouchableOpacity,
   Dimensions,
+  Image,
+  ImageBackground,
 } from 'react-native';
 
 const {height, width} = Dimensions.get('window')
@@ -15,20 +17,46 @@ const {height, width} = Dimensions.get('window')
 class Account extends Component {
   constructor(props){
     super(props)
-    const user = {}
+    const user = props.user || {}
     this.state = {
       user: user
     }
   }
 
   componentDidMount(){
+
+    AsyncStorage.getItem('user')
+      .then((data) => {
+        let user
+        if(data){
+          user = JSON.parse(data)
+        }
+
+        if(user && user.accessToken){
+          this.setState({
+            user: user
+          })
+        }
+      })
   }
 
   render(){
     const {user} = this.state
     return (
       <View style={styles.container}>
-        <View style={styles.avatarContainer}>
+      {
+        user.avatar
+        ? <TouchableOpacity style={styles.avatarContainer}>
+          <ImageBackground source={{uri: user.avatar}} style={styles.avatarContainer}>
+            <View style={styles.avatarBox}>
+              <Image
+                source={{uri: user.avatar}}
+                style={styles.avatar}/>
+              <Text style={styles.avatarTip}>戳这里换头像</Text>
+            </View>
+          </ImageBackground>
+        </TouchableOpacity>
+        : <View style={styles.avatarContainer}>
           <Text style={styles.avatarTip}>添加头像</Text>
           <TouchableOpacity style={styles.avatarBox}>
             <Icon
@@ -36,6 +64,7 @@ class Account extends Component {
               style={styles.plusIcon}/>
           </TouchableOpacity>
         </View>
+      }
       </View>
     )
   }
@@ -51,7 +80,21 @@ const styles = StyleSheet.create({
     height: 140,
     alignItems: 'center',
     justifyContent: 'center',
-    backgroundColor: '#eee'
+    backgroundColor: '#666'
+  },
+
+  avatarTip: {
+    color: '#fff',
+    backgroundColor: 'transparent',
+    justifyContent: 'center'
+  },
+
+  avatar: {
+    marginBottom: 15,
+    width: width * 0.2,
+    height: width * 0.2,
+    resizeMode: 'cover',
+    borderRadius: width * 0.1
   },
 
   avatarBox: {
